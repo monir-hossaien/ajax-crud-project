2359f55307977a5a1734774924604a9f9164b6db
Monir Hossain <monirhdigital@gmail.com>
2024-09-14 20:49:59 +0600

    version 5.0.0

--- js/update.js
+++ js/update.js
@@ -13,69 +13,60 @@ const id = urlParams.get("id");
 
 async function fillExistingData() {
   loader.style.display = "block";
-  
-
   let URL = `http://164.68.107.70:6060/api/v1/ReadProductById/${id}`;
   let res = await axios.get(URL);
-  
 
   if (res.status === 200) {
     loader.style.display = "none";
 
     let items = res.data["data"][0];
-    
+
     ProductNameInput.value = items.ProductName;
     ProductCodeInput.value = items.ProductCode;
     ProductImageInput.value = items.Img;
     UnitPriceInput.value = items.UnitPrice;
     ProductQtyInput.value = items.Qty;
     TotalPriceInput.value = items.TotalPrice;
-
-    
   }
 }
 
 fillExistingData();
 
-
-
 // data object
-let data = {
+
+async function updateData() {
+  let data = {
     ProductName: ProductNameInput.value,
     ProductCode: ProductCodeInput.value,
     Img: ProductImageInput.value,
     UnitPrice: UnitPriceInput.value,
     Qty: ProductQtyInput.value,
-    TotalPrice: TotalPriceInput.value
-};
+    TotalPrice: TotalPriceInput.value,
+  };
+  loader.style.display = "block";
+  let URL = `http://164.68.107.70:6060/api/v1/UpdateProduct/${id}`;
+  let res = await axios.post(URL, data);
+  console.log(data);
 
+  if (res.status === 200) {
+    loader.style.display = "none";
+    // toast message
+    toastMessage.innerHTML = "Update Successfully";
+    toastMessage.classList.add("msg-style-success");
 
-async function updateData(){
-    loader.style.display = "block";
-    let URL = `http://164.68.107.70:6060/api/v1/UpdateProduct/${id}`
-    let res = await axios.post(URL,data)
-    
-    if(res.status === 200){
+    setTimeout(() => {
+      (toastMessage.innerHTML = ""),
+        toastMessage.classList.remove("msg-style-success");
+    }, 2000);
 
-      loader.style.display = "none";
-        // toast message
-      toastMessage.innerHTML = "Update Successfully";
-      toastMessage.classList.add("msg-style-success");
+    // window.location = "/list.html";
+  } else {
+    toastMessage.innerHTML = "Request failed";
+    toastMessage.classList.add("msg-style-fail");
 
-      setTimeout(() => {
-        (toastMessage.innerHTML = ""),
-          toastMessage.classList.remove("msg-style-success");
-      }, 2000);
-    }
-    else{
-        
-        toastMessage.innerHTML = "Request failed";
-        toastMessage.classList.add("msg-style-fail");
-
-      setTimeout(() => {
-        toastMessage.innerHTML = "",
-          toastMessage.classList.remove("msg-style-fail");
-      }, 2000);
-    }
-
+    setTimeout(() => {
+      (toastMessage.innerHTML = ""),
+        toastMessage.classList.remove("msg-style-fail");
+    }, 2000);
+  }
 }
